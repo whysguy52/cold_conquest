7092f1f18be0b6fac28e2347a05aac6a32f30334
Ben Eng <whysguy52@gmail.com>
2021-02-08 12:40:25 -0600

    Added Spawn Point
    ToDo: script the ready function on test level. Make it universal!

---
+++ scenes/Levels/LevelSetupScript.gd
@@ -0,0 +1,14 @@
+extends Spatial
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+#load in all ships and assign all platers to the roles. 
+#--Make that individual the master of their node based on role
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
