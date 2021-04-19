d1522dbc7e2561be6dd50ea2e69ecb6004610db0
Ben Eng <whysguy52@gmail.com>
2021-04-18 23:28:47 -0500

    Thruster Slider Working
    Thruster slider implemented and working

--- scenes/Ships/Battleship/Scripts/BattleShipMovement.gd
+++ scenes/Ships/Battleship/Scripts/BattleShipMovement.gd
@@ -1,15 +1,19 @@
 extends KinematicBody
-#Translate this node and rotate the child node "ShipBody"
+
 
 var shipPosition
 var role
 var maxSpeed = 5
+var currentSpeed = 0
+var ThrusterHUDSlider
+var warpFactor
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	role = NetworkManager.myPlayerData["role"]
 	shipPosition = $ShipBody
+	ThrusterHUDSlider = get_node("PilotHud/Panel/ThrusterSlider")
 
 func _physics_process(delta):
 	if role != "pilot":
@@ -17,8 +21,20 @@ func _physics_process(delta):
 	move_ship_forward(delta)
 
 func move_ship_forward(delta):
-	move_and_slide(shipPosition.transform.basis.x.normalized() * maxSpeed)
-	rpc("remote_move_ship", transform)
+	move_and_slide(shipPosition.transform.basis.x.normalized() * currentSpeed)
 
-remote func remote_move_ship(transform):
-	self.transform = transform
+func speed_up():
+	currentSpeed += 0.05
+	update_thruster_slider()
+
+func speed_down():
+	currentSpeed -= 0.05
+	update_thruster_slider()
+
+func update_thruster_slider():
+	currentSpeed = clamp(currentSpeed, -0.5, 5)
+	ThrusterHUDSlider.value = currentSpeed / maxSpeed * 100 # this is to get a number between 0 and 100
+
+
+func _on_ThrusterSlider_value_changed(value):
+	currentSpeed = value / 100 * 5 
--- scenes/Ships/Battleship/Scripts/Pilot.gd
+++ scenes/Ships/Battleship/Scripts/Pilot.gd
@@ -3,26 +3,33 @@ extends Spatial
 
 var role
 
+#other objects
 var CameraNod
-var ShipBody
+var ShipTurnObj
+var ShipTranslateObj
 var HUD
 var orientationLocked = false
+var ThrusterSlider
 
 var MOUSE_SENSITIVITY = 0.05
 
+#booleans
 var isDoneRotating = true
 var isUpdating = false
+var isInHudPanel = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	role = NetworkManager.myPlayerData["role"]
 	if role != "pilot":
 		return
-	ShipBody = get_parent().get_node("ShipBody")
+	ShipTurnObj = get_parent().get_node("ShipBody")
+	ShipTranslateObj = get_parent()
 	CameraNod = $CameraNod
 	$CameraNod/PilotCam.make_current()
 	HUD = get_parent().get_node("PilotHud")
 	HUD.visible = true
+	ThrusterSlider = get_parent().get_node("PilotHud/Panel/ThrusterSlider")
 
 func _physics_process(delta):
 	if role != "pilot":
@@ -31,30 +38,39 @@ func _physics_process(delta):
 
 func process_input():
 	
-	#Check if it's a mouse button
-	if Input.is_action_pressed("LMB") or Input.is_action_pressed("RMB"):
-		
-		#Lock the mouse
-		if Input.get_mouse_mode() == Input.MOUSE_MODE_VISIBLE:
-			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-		#if it's RMB, tell ship to update cam direction
-		if Input.is_action_pressed("RMB"):
-			isUpdating = true
-			ShipBody.turn_ship(isUpdating,orientationLocked)
-			isDoneRotating = false
-		
-	else:
-		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	#check the throttle
+	if Input.is_action_pressed("ui_up"):
+		ShipTranslateObj.speed_up()
+	
+	if Input.is_action_pressed("ui_down"):
+		ShipTranslateObj.speed_down()
+	
+	
+	
+	if isInHudPanel == false :
+		if Input.is_action_pressed("LMB") or Input.is_action_pressed("RMB"):
+			#Lock the mouse
+			if Input.get_mouse_mode() == Input.MOUSE_MODE_VISIBLE:
+				Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+			#if it's RMB, tell ship to update cam direction
+			if Input.is_action_pressed("RMB"):
+				isUpdating = true
+				ShipTurnObj.turn_ship(isUpdating,orientationLocked)
+				isDoneRotating = false
+			
+		else:
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	
 	
 	if !isDoneRotating and !Input.is_action_pressed("RMB"):
 			isUpdating = false
-			ShipBody.turn_ship(false,orientationLocked)
+			ShipTurnObj.turn_ship(false,orientationLocked)
 	
 	if Input.is_action_pressed("ui_roll_left") or Input.is_action_pressed("ui_roll_right"):
-		ShipBody.roll_ship()
+		ShipTurnObj.roll_ship()
 	
 	#this may be overly redundant
-	ShipBody.transform.orthonormalized()
+	ShipTurnObj.transform.orthonormalized()
 
 func _input(event):
 	if role != "pilot":
@@ -70,3 +86,10 @@ func _on_CheckButton_toggled(button_pressed):
 	else:
 		orientationLocked = false
 	isUpdating = true
+
+
+func _on_pilot_hud_mouse_entered():
+	isInHudPanel = true
+
+func _on_pilot_hud_mouse_exited():
+	isInHudPanel = false
