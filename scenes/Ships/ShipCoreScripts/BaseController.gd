96adb0619efc54d99dedb322fc3b1548cd3f9aee
Ben Eng <whysguy52@gmail.com>
2022-05-15 00:41:47 -0500

    control pilot
    i need to think of how i want to do the pilot controls

--- scenes/Ships/ShipCoreScripts/BaseController.gd
+++ scenes/Ships/ShipCoreScripts/BaseController.gd
@@ -12,7 +12,7 @@ func _ready():
     
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
     if Input.is_action_pressed("LMB"):
         Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
     elif Input.is_action_just_released("LMB"):
--- scenes/Ships/ShipCoreScripts/StationController_Pilot.gd
+++ scenes/Ships/ShipCoreScripts/StationController_Pilot.gd
@@ -10,6 +10,9 @@ extends "res://scenes/Ships/ShipCoreScripts/BaseController.gd"
 func _ready():
     pass # Replace with function body.
 
+func _physics_process(_delta):
+    pass
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
