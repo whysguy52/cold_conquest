364f226db8ec051e66642d5d2cc193d37bc1df4d
Ben Eng <whysguy52@gmail.com>
2022-06-24 21:13:41 -0500

    slerp is mostly correct
    Now i need to figure out what to do when the player holds the RMB

--- scenes/Ships/ShipCoreScripts/ShipRotator.gd
+++ scenes/Ships/ShipCoreScripts/ShipRotator.gd
@@ -1,29 +1,35 @@
 extends KinematicBody
 
 
-#var weight = 1.0
+var weight = 1.0
 var rotationSpeed = 0.05
 var isRotating:bool
+var startAngle:Basis
 var targetAngle:Basis
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
     targetAngle = global_transform.basis
 
-func _physics_process(_delta):
-    #works but not as intended
-    if targetAngle.is_equal_approx(transform.basis):
-        transform.basis = transform.basis.slerp(targetAngle,1).orthonormalized()
+func _physics_process(delta):
+    if isRotating == true and weight <1:
+        weight += delta
+        turnShip()
     else:
-        transform.basis = transform.basis.slerp(targetAngle, rotationSpeed).orthonormalized()
+        isRotating = false
+        weight = 1
+        transform.basis = startAngle.slerp(targetAngle,1).orthonormalized()
+    print_debug(isRotating)
 
 
 func set_new_angle(camAngle:Basis):
+    weight = 0.0
     isRotating = true
     targetAngle = camAngle
+    startAngle = transform.basis.orthonormalized()
 
 func turnShip():
-    transform.basis = transform.basis.slerp(targetAngle, rotationSpeed).orthonormalized()
+    transform.basis = startAngle.slerp(targetAngle, weight).orthonormalized()
 
 
 
